b1af87dad2645f158bf0af4979cbd0a88cc0156b
Rick Lan <ricklan@gmail.com>
2019-05-22 00:15:28 +1000

    localisation script

--- apk-localisation/localisation.py
+++ apk-localisation/localisation.py
@@ -1,25 +1,29 @@
 import os
 import sys
 import json
+import io
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-with open('./strings.json', 'r') as f:
-    ds = json.load(f)
-
 # we need language and ip
 if len(sys.argv) < 2:
-    print("Usage: install.py <locale> <EON IP>")
-    print("Available locale: '%s'" % '\', \''.join(ds["locales"]))
+    print("Usage: install.py <locale> <json translation file>")
     os._exit(1)
 
 locale = sys.argv[1]
+json_file = sys.argv[2]
+
+with open(json_file, 'r') as f:
+    ds = json.load(f)
+
+# print(ds)
+# os._exit(1)
 if os.path.isfile('index.android.bundle'):
     src = io.open('index.android.bundle', 'r', encoding='utf-8')
     content = src.read()
     print("Patching strings")
-    for string in strings:
+    for string in ds:
         content = content.replace(string['en'], string[locale])
-    dest = open('./tmp/apk_src/assets/index.android.bundle', 'w')
+    dest = open('index.android.bundle.new', 'w')
     dest.write(content)
