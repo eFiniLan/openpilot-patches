ed6ac662f097bee61d6fa9c04c59b1a0db31d485
Rick Lan <ricklan@gmail.com>
2019-05-22 23:49:22 +1000

    Add more zh-TW translation for ai.comma.plus.offroad.apk

--- apk-localisation/localisation.py
+++ apk-localisation/localisation.py
@@ -7,23 +7,24 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 
 # we need language and ip
-if len(sys.argv) < 2:
-    print("Usage: install.py <locale> <json translation file>")
+if len(sys.argv) < 3:
+    print("Usage: install.py <locale> <json translation file> <index.android.bundle file>")
     os._exit(1)
 
 locale = sys.argv[1]
 json_file = sys.argv[2]
+bundle_file = sys.argv[3]
 
 with open(json_file, 'r') as f:
     ds = json.load(f)
 
 # print(ds)
 # os._exit(1)
-if os.path.isfile('index.android.bundle'):
-    src = io.open('index.android.bundle', 'r', encoding='utf-8')
+if os.path.isfile(bundle_file):
+    src = io.open(bundle_file, 'r', encoding='utf-8')
     content = src.read()
     print("Patching strings")
     for string in ds:
         content = content.replace(string['en'], string[locale])
-    dest = open('index.android.bundle.new', 'w')
+    dest = open(bundle_file, 'w')
     dest.write(content)
