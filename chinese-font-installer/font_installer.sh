8ca7ec37b206715f2788494ee085acd151ed0851
Rick Lan <ricklan@gmail.com>
2019-06-24 15:04:13 +1000

    update font installer script

--- chinese-font-installer/font_installer.sh
+++ chinese-font-installer/font_installer.sh
@@ -1,27 +1,49 @@
 #!/usr/bin/bash
 
-## Android 端設定成中文介面 (zh-TW = 繁中, zh-CN = 简中)
+## Android 端設 Z H~P中 V~G K ]  (zh-TW =  A中, zh-CN =  @中)
 lang=zh-TW
-update_font_conf=0
+update_font_reg=0
+update_font_bold=0
+remove_old_font=0
 
+# check regular font
 if [ ! -f "/system/fonts/Miui-Regular.ttf" ]; then
-    update_font_conf=1
-    mount -o remount,rw /system && curl -o /system/fonts/Miui-Regular.ttf https://raw.githubusercontent.com/eFiniLan/openpilot-patches/master/chinese-font-installer/assets/Miui-Regular.ttf
+    update_font_reg=1
 fi
 
+# check bold font
 if [ ! -f "/system/fonts/Miui-Bold.ttf" ]; then
-    update_font_conf=1
-    mount -o remount,rw /system && curl -o /system/fonts/Miui-Bold.ttf https://raw.githubusercontent.com/eFiniLan/openpilot-patches/master/chinese-font-installer/assets/Miui-Bold.ttf
+    update_font_bold=1
 fi
 
+# check droidsans font
 if ls /system/fonts/DroidSansFallback*.ttf 1> /dev/null 2>&1; then
-    mount -o remount,rw /system && rm -fr /system/fonts/DroidSansFallback*.ttf
+    remove_old_font=1
 fi
 
-if [[ $update_font_conf -eq "1" ]]; then
-    curl -o /system/etc/fonts.xml https://raw.githubusercontent.com/eFiniLan/openpilot-patches/master/chinese-font-installer/assets/fonts.xml
-    chmod 644 /system/etc/fonts.xml
+if [ $update_font_reg -eq "1" ] || [ $update_font_bold -eq "1" ] || [ $remove_old_font -eq "1" ]; then
+    # sleep 5 secs in case, make sure the /system is remountable
+    sleep 5
+    mount -o remount,rw /system
+    if [ $update_font_reg -eq "1" ] || [ $update_font_bold -eq "1" ]; then
+        # download regular font
+        if [ $update_font_reg -eq "1" ]; then
+            curl -o /system/fonts/Miui-Regular.ttf https://raw.githubusercontent.com/eFiniLan/openpilot-patches/master/chinese-font-installer/assets/Miui-Regular.ttf
+        fi
+        # download bold font
+        if [ $update_font_bold -eq "1" ]; then
+            curl -o /system/fonts/Miui-Bold.ttf https://raw.githubusercontent.com/eFiniLan/openpilot-patches/master/chinese-font-installer/assets/Miui-Bold.ttf
+        fi
+        # dont new font mapping
+        curl -o /system/etc/fonts.xml https://raw.githubusercontent.com/eFiniLan/openpilot-patches/master/chinese-font-installer/assets/fonts.xml
+        chmod 644 /system/etc/fonts.xml
+        chmod 644 /system/fonts/Miui-*
+    fi
+    # remove driodsans font
+    if [ $remove_old_font -eq "1" ]; then
+        rm -fr /system/fonts/DroidSansFallback*.ttf
+    fi
     mount -o remount,r /system
+    # change system locale
+    setprop persist.sys.locale $lang
 fi
-
-setprop persist.sys.locale $lang
